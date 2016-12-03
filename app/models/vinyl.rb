b8cf6040756ab727edf904ab123b4b379ae3b6c3
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-12-03 09:51:21 +0100

    simple form added

--- app/models/vinyl.rb
+++ app/models/vinyl.rb
@@ -1,6 +1,6 @@
 class Vinyl < ApplicationRecord
   def self.import(file)
-    CSV.foreach(file.path, headers: true) do |row|
+    CSV.foreach(file.path, headers: true, :quote_char => "\'") do |row|
       Vinyl.create! row.to_hash
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161202155219) do
+ActiveRecord::Schema.define(version: 20161203084228) do
 
   create_table "vinyls", force: :cascade do |t|
     t.string   "catalog"
